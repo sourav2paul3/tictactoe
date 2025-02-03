28c8e5b004012c7607750a227a0a2f81fc7c3642
sourav2paul3 <40515551+sourav2paul3@users.noreply.github.com>
2025-02-03 19:09:39 +0530

    Update GameOverPopUp.tsx

--- tictactoe/src/components/GameOverPopUp.tsx
+++ tictactoe/src/components/GameOverPopUp.tsx
@@ -4,12 +4,15 @@ const GameOverPopUp = ({ winner }: { winner: string | null }) => {
   const { resetGame } = useTicTacToe();
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
-      <div className="bg-gray-400 bg-opacity-70 backdrop-blur p-8 rounded-lg shadow-lg">
-        <div className="text-xl text-white">
+      <div
+        className="bg-gray-400 bg-opacity-70 backdrop-blur p-8 rounded-lg shadow-lg 
+        flex flex-col items-center justify-center"
+      >
+        <div className="text-xl text-white text-center mb-4">
           {winner === "Draw" ? "It's a Draw!" : `Player ${winner} Wins!`}
         </div>
         <button
-          className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md"
+          className="px-6 py-2 bg-blue-500 text-white rounded-md max-w-[150px] mt-4"
           onClick={resetGame}
         >
           Play Again
